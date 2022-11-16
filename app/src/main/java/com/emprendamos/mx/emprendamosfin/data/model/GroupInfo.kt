370033ac3dbc2395368bb8829933b85acf9c9429
marcos.moreno <marcos.moreno.gm@gmailcom>
2022-11-16 17:21:37 -0600

    first commit

---
+++ app/src/main/java/com/emprendamos/mx/emprendamosfin/data/model/GroupInfo.kt
@@ -0,0 +1,7 @@
+package com.emprendamos.mx.emprendamosfin.data.model
+
+import com.emprendamos.mx.emprendamosfin.data.database.repository.entities.Group
+
+class GroupInfo {
+    var groups : Map<Int, Group>? = null
+}
